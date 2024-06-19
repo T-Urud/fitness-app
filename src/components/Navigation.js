2351fae203521ad9c3446dd116198cdfffb29fae
urud.theo@gmail.com <urud.theo@gmail.com>
2024-06-19 19:08:17 +0200

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-home"
+            >
+              <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+              <polyline points="9 22 9 12 15 12 15 22" />
+            </svg>
+            <p>Home page</p>
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-user-round-plus"
+            >
+              <path d="M2 21a8 8 0 0 1 13.292-6" />
+              <circle cx="10" cy="8" r="5" />
+              <path d="M19 16v6" />
+              <path d="M22 19h-6" />
+            </svg>
+            <p>Friends</p>
+          </li>
+          <li class="actived">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-circle-user-round"
+            >
+              <path d="M18 20a6 6 0 0 0-12 0" />
+              <circle cx="12" cy="10" r="4" />
+              <circle cx="12" cy="12" r="10" />
+            </svg>
+            <p>Profile</p>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navigation;
---
+++ src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const Profile = () => {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+};
+
+export default Profile;
